afec7d2c59c1178b5b05f25b1667b097c3ffead9
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-01-21 23:29:15 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = (text) => {
+    // will not check strings which is longer than the half of the original string
+    for(let i=1 ; i<text.length/2 ; i++){
+        // check strings which can divide original strings without reminder
+        if(text.length % i === 0){
+            // repeated: potential repeated string
+            let repeated = text.substring(0,i);
+            // we will set this flag false if it is not repeated on every step
+            let flag_repeated = true;
+
+            // start scanning the original string
+            for(let j=0 ; j<text.length-repeated.length+1 ; j+=repeated.length){
+                // part of the original string to be compared with potential repeated
+                let compare = text.substring(j,j+repeated.length);
+
+                if(compare === repeated){
+                    console.log("repeated: " + repeated + " is compared by: " + compare + " index: " + j);
+                }else{
+                    flag_repeated = false;
+                    break;
+                }
+            }
+
+            if(flag_repeated){
+                console.log("finally repeated is: " + repeated);
+                let repeatCount = text.length / repeated.length;
+                return {repeated: repeated, 
+                        count: repeatCount};
+            }
+        }
+    }
+}
