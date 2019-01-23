5f88655b0d5c1e4aa4247f267cc8f328a81e871c
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-01-23 19:53:54 +0300

    added eslint

--- src/index.js
+++ src/index.js
@@ -1,41 +1,45 @@
-module.exports = (text) => {
-    // type check
-    if(typeof text === "undefined"){
-        return text;
-    }else if(typeof text !== "string"){
-        text = text.toString();
-    }
+module.exports = (input) => {
+  let text = input;
+  // type check
+  if (typeof text === 'undefined') {
+    return text;
+  } if (typeof text !== 'string') {
+    text = text.toString();
+  }
 
-    // will not check strings which is longer than the half of the original string
-    for(let i=1 ; i<=text.length/2 ; i++){
-        // check strings which can divide original strings without reminder
-        if(text.length % i === 0){
-            // repeated: potential repeated string
-            let repeated = text.substring(0,i);
-            // we will set this flag false if it is not repeated on every step
-            let flag_repeated = true;
+  // will not check strings which is longer than the half of the original string
+  for (let i = 1; i <= text.length / 2; i += 1) {
+    // check strings which can divide original strings without reminder
+    if (text.length % i === 0) {
+      // repeated: potential repeated string
+      const repeated = text.substring(0, i);
+      // we will set this flag false if it is not repeated on every step
+      let isRepeated = true;
 
-            // start scanning the original string
-            for(let j=0 ; j<text.length-repeated.length+1 ; j+=repeated.length){
-                // part of the original string to be compared with potential repeated
-                let compare = text.substring(j,j+repeated.length);
+      // start scanning the original string
+      for (let j = 0; j < text.length - repeated.length + 1; j += repeated.length) {
+        // part of the original string to be compared with potential repeated
+        const compare = text.substring(j, j + repeated.length);
 
-                if(compare === repeated){
-                }else{
-                    flag_repeated = false;
-                    break;
-                }
-            }
-
-            if(flag_repeated){
-                let repeatCount = text.length / repeated.length;
-                return {repeated: repeated, 
-                        count: repeatCount};
-            }
+        if (compare !== repeated) {
+          isRepeated = false;
+          break;
         }
-    }
+      }
 
-    // if no repeats found, then return the input itself
-    return {repeated: text, 
-             count: 1};
-}
+      if (isRepeated) {
+        const repeatCount = text.length / repeated.length;
+        return {
+          repeated,
+          count: repeatCount,
+        };
+      }
+    }
+  }
+
+  // if no repeats found, then return the input itself
+  return {
+    repeated: text,
+    count: 1,
+  };
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,36 +1,41 @@
 const reverseRepeat = require('../src/index');
 
 test('basic test', () => {
-    expect(reverseRepeat("qwe".repeat(18))).toEqual({
-        repeated: "qwe", 
-        count: 18});
+  expect(reverseRepeat('qwe'.repeat(18))).toEqual({
+    repeated: 'qwe',
+    count: 18,
   });
+});
 
 test('test with integer value', () => {
-    expect(reverseRepeat(121212)).toEqual({
-        repeated: "12", 
-        count: 3});
+  expect(reverseRepeat(121212)).toEqual({
+    repeated: '12',
+    count: 3,
   });
+});
 
 
 test('test with not repeated input', () => {
-    expect(reverseRepeat("there is no repeat here")).toEqual({
-        repeated: "there is no repeat here", 
-        count: 1});
+  expect(reverseRepeat('there is no repeat here')).toEqual({
+    repeated: 'there is no repeat here',
+    count: 1,
   });
+});
 
 test('test with 2 repeats', () => {
-    expect(reverseRepeat("qwe".repeat(2))).toEqual({
-        repeated: "qwe", 
-        count: 2});
+  expect(reverseRepeat('qwe'.repeat(2))).toEqual({
+    repeated: 'qwe',
+    count: 2,
   });
+});
 
 test('empty input', () => {
-    expect(reverseRepeat("")).toEqual({
-        repeated: "", 
-        count: 1});
+  expect(reverseRepeat('')).toEqual({
+    repeated: '',
+    count: 1,
   });
+});
 
-  test('undefined input', () => {
-    expect(reverseRepeat(undefined)).toEqual(undefined);
-  });
+test('undefined input', () => {
+  expect(reverseRepeat(undefined)).toEqual(undefined);
+});
